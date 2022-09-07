19c4463397312d91dbba64d6e1c9712b43832dc8
Belendia Serda <belendia@gmail.com>
2022-09-07 07:23:20 +0300

    Start ODK collect by clicking one of the sample rows

--- lib/presentation/widgets/sample_row.dart
+++ lib/presentation/widgets/sample_row.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:android_intent_plus/android_intent.dart';
+import 'dart:io' show Platform;
 
 import '../../data/dao/sample_dao.dart';
 import '../../data/model/sample.dart';
@@ -15,81 +17,94 @@ class SampleRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ListTile(
-          leading: Checkbox(
-              checkColor: Colors.white,
-              activeColor: Colors.grey,
-              value: sample.completed,
-              onChanged: (val) {
-                context
-                    .read<SampleBloc>()
-                    .add(ToogleSampleEvent(sample.key, placeId));
-              }),
-          title: Padding(
-            padding: const EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  sample.sample.toString(),
-                  style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      color: sample.completed ? Colors.grey : Colors.black,
-                      decoration:
-                          sample.completed ? TextDecoration.lineThrough : null),
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                ),
-                Text(
-                  sample.name,
-                  style: TextStyle(
-                      fontSize: 16.0,
-                      decoration: sample.completed == true
-                          ? TextDecoration.lineThrough
-                          : null),
-                ),
-              ],
+    return GestureDetector(
+      child: Column(
+        children: [
+          ListTile(
+            leading: Checkbox(
+                checkColor: Colors.white,
+                activeColor: Colors.grey,
+                value: sample.completed,
+                onChanged: (val) {
+                  context
+                      .read<SampleBloc>()
+                      .add(ToogleSampleEvent(sample.key, placeId));
+                }),
+            title: Padding(
+              padding: const EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    sample.sample.toString(),
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: sample.completed ? Colors.grey : Colors.black,
+                        decoration: sample.completed
+                            ? TextDecoration.lineThrough
+                            : null),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  ),
+                  Text(
+                    sample.name,
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        decoration: sample.completed == true
+                            ? TextDecoration.lineThrough
+                            : null),
+                  ),
+                ],
+              ),
             ),
-          ),
-          enabled: sample.completed == false,
-          trailing: sample.completed
-              ? null
-              : IconButton(
-                  icon: const Icon(Icons.edit),
-                  tooltip: 'Edit',
-                  onPressed: () {
-                    if (sample.completed == false) {
-                      showDialog(
-                        context: context,
-                        builder: (context) => Dialog(
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10.0),
+            enabled: sample.completed == false,
+            trailing: sample.completed
+                ? null
+                : IconButton(
+                    icon: const Icon(Icons.edit),
+                    tooltip: 'Edit',
+                    onPressed: () {
+                      if (sample.completed == false) {
+                        showDialog(
+                          context: context,
+                          builder: (context) => Dialog(
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10.0),
+                              ),
                             ),
+                            child: RenameSampleName(
+                                sampleId: sample.key, name: sample.name),
                           ),
-                          child: RenameSampleName(
-                              sampleId: sample.key, name: sample.name),
-                        ),
-                      ).then((result) {
-                        if (result != null) {
-                          final s = SampleDAO.fromJson(result);
-                          context.read<SampleBloc>().add(
-                              EditSampleEvent(s.sampleId!, s.name!, placeId));
-                        }
-                      });
-                    }
-                  },
-                ),
-        ),
-        const Divider(
-          color: Colors.grey,
-        ),
-        const Padding(padding: EdgeInsets.symmetric(vertical: 1.0))
-      ],
+                        ).then((result) {
+                          if (result != null) {
+                            final s = SampleDAO.fromJson(result);
+                            context.read<SampleBloc>().add(
+                                EditSampleEvent(s.sampleId!, s.name!, placeId));
+                          }
+                        });
+                      }
+                    },
+                  ),
+          ),
+          const Divider(
+            color: Colors.grey,
+          ),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 1.0))
+        ],
+      ),
+      onTap: () async {
+        if (Platform.isAndroid && sample.completed == false) {
+          // start ODK collect
+          const intent = AndroidIntent(
+            action: 'action_view',
+            type: 'vnd.android.cursor.dir/vnd.odk.form',
+          );
+          await intent.launch();
+        }
+      },
     );
   }
 }
